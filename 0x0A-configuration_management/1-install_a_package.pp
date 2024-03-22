95e4f9fc9418ddd7abbe6a7af736386bebc26a84
soukainanachib <soukaina.nachib23@gmail.com>
2024-03-22 22:54:50 +0000

    fix requirement

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# To install Flask version 2.1.0 using Puppet
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
